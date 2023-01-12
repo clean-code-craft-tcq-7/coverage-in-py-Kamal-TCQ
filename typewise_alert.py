00f31b101bb3b86d894a8c3967c6365b70dd413e
Kamal-TCQ <115541263+Kamal-TCQ@users.noreply.github.com>
2023-01-12 13:21:09 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,33 +1,34 @@
-def infer_breach(value, lower_limit, upper_limit):
-    if value < lower_limit:
-        return 'TOO_LOW'
-    if value > upper_limit:
-        return 'TOO_HIGH'
-    return 'NORMAL'
+import unittest
 
-limits = {
-    'PASSIVE_COOLING': (0, 35),
-    'HI_ACTIVE_COOLING': (0, 45),
-    'MED_ACTIVE_COOLING': (0, 40),
-}
+import typewise_alert
 
-def check_and_alert(alert_target, battery_char, temperature_in_c):
-    lower_limit, upper_limit = limits[battery_char['coolingType']]
-    breach_type = infer_breach(temperature_in_c, lower_limit, upper_limit)
-    if alert_target == 'TO_CONTROLLER':
-        send_to_controller(breach_type)
-    elif alert_target == 'TO_EMAIL':
-        send_to_email(breach_type)
-		
-def send_to_controller(breach_type):
-    header = 0xfeed
-    print(f'{header}, {breach_type}')
+class TypewiseTest(unittest.TestCase):
+    def test_infers_breach_as_per_limits(self):
+        self.assertEqual(typewise_alert.infer_breach(20, 50, 100), 'TOO_LOW')
+        self.assertEqual(typewise_alert.infer_breach(150, 50, 100), 'TOO_HIGH')
+        self.assertEqual(typewise_alert.infer_breach(50, 50, 100), 'NORMAL')
+        self.assertEqual(typewise_alert.infer_breach(75, 50, 100), 'NORMAL')
+        self.assertEqual(typewise_alert.infer_breach(49, 50, 100), 'TOO_LOW')
+        self.assertEqual(typewise_alert.infer_breach(101, 50, 100), 'TOO_HIGH')
 
-def send_to_email(breach_type):
-    recepient = "a.b@c.com"
-    if breach_type == 'TOO_LOW':
-        print(f'To: {recepient}')
-        print('Hi, the temperature is too low')
-    elif breach_type == 'TOO_HIGH':
-        print(f'To: {recepient}')
-        print('Hi, the temperature is too high')
+    def test_check_and_alert_with_different_alert_targets(self):
+        alert_target = 'TO_CONTROLLER'
+        battery_char = {'coolingType': 'PASSIVE_COOLING'}
+        temperature_in_c = 30
+        breach_type, message = typewise_alert.check_and_alert(alert_target, battery_char, temperature_in_c)
+        self.assertEqual(breach_type, 'NORMAL')
+        self.assertEqual(message, '0xfeed, NORMAL')
+
+        alert_target = 'TO_EMAIL'
+        temperature_in_c = 50
+        breach_type, message = typewise_alert.check_and_alert(alert_target, battery_char, temperature_in_c)
+        self.assertEqual(breach_type, 'TOO_HIGH')
+        self.assertEqual(message, 'a.b@c.com, Hi, the temperature is too high')
+
+    def test_with_limits_not_defined(self):
+        battery_char = {'coolingType': 'UNDEFINED_COOLING'}
+        temperature_in_c = 30
+        self.assertRaises(KeyError, typewise_alert.check_and_alert, 'TO_CONTROLLER', battery_char, temperature_in_c)
+
+if __name__ == '__main__':
+    unittest.main()
