d99889ea693b665fea4394721a511ff81c573f72
Kamal-TCQ <115541263+Kamal-TCQ@users.noreply.github.com>
2023-01-12 13:37:45 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,15 +1,35 @@
-import unittest
+def infer_breach(value, lowerLimit, upperLimit):
+  if value < lowerLimit:
+    return 'TOO_LOW'
+  if value > upperLimit:
+    return 'TOO_HIGH'
+  return 'NORMAL'
 
-import typewise_alert
+limits = {
+    'PASSIVE_COOLING': (0, 35),
+    'HI_ACTIVE_COOLING': (0, 45),
+    'MED_ACTIVE_COOLING': (0, 40),
+}
 
-class TypewiseTest(unittest.TestCase):
-    def test_infers_breach_as_per_limits(self):
-        self.assertEqual(typewise_alert.infer_breach(20, 50, 100), 'TOO_LOW')
-        self.assertEqual(typewise_alert.infer_breach(150, 50, 100), 'TOO_HIGH')
-        self.assertEqual(typewise_alert.infer_breach(50, 50, 100), 'NORMAL')
-        self.assertEqual(typewise_alert.infer_breach(75, 50, 100), 'NORMAL')
-        self.assertEqual(typewise_alert.infer_breach(49, 50, 100), 'TOO_LOW')
-        self.assertEqual(typewise_alert.infer_breach(101, 50, 100), 'TOO_HIGH')
+def check_and_alert(alertTarget, batteryChar, temperatureInC):
+  breachType =\
+    classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
+  if alertTarget == 'TO_CONTROLLER':
+    send_to_controller(breachType)
+  elif alertTarget == 'TO_EMAIL':
+    send_to_email(breachType)
 
-if __name__ == '__main__':
-    unittest.main()
+
+def send_to_controller(breachType):
+  header = 0xfeed
+  print(f'{header}, {breachType}')
+
+
+def send_to_email(breachType):
+  recepient = "a.b@c.com"
+  if breachType == 'TOO_LOW':
+    print(f'To: {recepient}')
+    print('Hi, the temperature is too low')
+  elif breachType == 'TOO_HIGH':
+    print(f'To: {recepient}')
+    print('Hi, the temperature is too high')
